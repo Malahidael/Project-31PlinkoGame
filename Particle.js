503904dba5c5564103cd6b1837aca892e8a8bf80
Malahidael <83966559+Malahidael@users.noreply.github.com>
2022-01-17 07:37:14 -0800

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,29 @@
+class Particles {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, this.r, options);
+        //give color property
+        this.color= color(random(62,62),random(159,159),random(195,195));
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
